834e8d814361f2ec86b28f94fe01add60a46c4c7
Tejas <panchaltejas253@gmail.com>
2026-01-21 14:08:59 +0530

    Starting with the project

---
+++ BackEnd/Driver_serv/src/test/java/com/example/Driver_serv/DriverServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Driver_serv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
