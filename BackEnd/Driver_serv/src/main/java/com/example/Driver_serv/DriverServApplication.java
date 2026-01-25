82e5de3edd5a35447f1ac80a191f5a5b9805ded1
Tejas <panchaltejas253@gmail.com>
2026-01-25 16:39:08 +0530

    Connection between Pickup_request and Tracking_service

--- BackEnd/Driver_serv/src/main/java/com/example/Driver_serv/DriverServApplication.java
+++ BackEnd/Driver_serv/src/main/java/com/example/Driver_serv/DriverServApplication.java
@@ -10,6 +10,7 @@ public class DriverServApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DriverServApplication.class, args);
+        System.out.println("Connection is successful");
 	}
 
 }
